06200755f5b9805aa6fd0d0f29a2d183467b795f
shruthi <95839061+adlashruthi@users.noreply.github.com>
2022-10-16 12:25:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
+import {BiCalculator} from 'react-icons/bi';
 
-function App() {
+const App = () => {
+  const [input,setInput] = useState("");
+  const [result,setResult] = useState(0);
+  const handler = e =>{
+    setInput(e.target.value);
+  }
+  const resultRender = () => {
+
+
+    try {
+      setResult(eval(input))
+
+    }
+    catch {
+      setResult("Error")
+
+    }
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <center>
+        <h2 className="heading">Calculator App <BiCalculator /></h2>
+        <input type="text" value={input} name="input" onChange={handler} />
+        <br />
+        {/*<button className="button-result" onClick={() => setResult(eval(input))}>Result</button> */}
+         <button className="button-result" onClick={resultRender}>Result</button> 
+        <h4 className="result-heading">Result is : {result}</h4>
+
+        <button onClick={() => setInput(input+'1')}>1</button>
+        <button onClick={() => setInput(input+'2')}>2</button>
+        <button onClick={() => setInput(input+'3')}>3</button>
+        <button onClick={() => setInput(input+'4')}>4</button>
+        <button onClick={() => setInput(input+'5')}>5</button> <br />
+        
+        <button onClick={() => setInput(input+'6')}>6</button>
+        <button onClick={() => setInput(input+'7')}>7</button>
+        <button onClick={() => setInput(input+'8')}>8</button>
+        <button onClick={() => setInput(input+'9')}>9</button>
+        <button onClick={() => setInput(input+'0')}>0</button> <br />
+
+        <button onClick={() => setInput(input+'+')}>+</button>
+        <button onClick={() => setInput(input+'-')}>-</button>
+        <button onClick={() => setInput(input+'*')}>*</button>
+        <button onClick={() => setInput(input+'/')}>/</button>
+    
+        <button onClick={() => {setInput('');setResult(0)}}>C</button><br /> 
+
+      </center>
+      <br />
     </div>
-  );
+  )
 }
 
 export default App;
